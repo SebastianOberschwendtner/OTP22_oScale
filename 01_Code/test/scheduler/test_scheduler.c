4be8a38d74b0ee6c329201f77bf95dff4d616ed3
Sebastian Oberschwendtner <sebastian.oberschwendtner@gmail.com>
2021-04-17 18:59:47 +0200

    :8ball: Addes tests for scheduler.
    With these tests, the coverage of the scheduler library is completed.
    The main scheduling principle is covered. The library does not capture
    overflows or skipped executions. But for this simple scheduler, this seems
    not too important.

--- 01_Code/test/scheduler/test_scheduler.c
+++ 01_Code/test/scheduler/test_scheduler.c
@@ -74,63 +74,99 @@ void test_set_task(void)
 };
 
 /**
+ * @brief Test the task counter and whether run flags are reset after reading.
+ * @details unit test
+ */
+void test_count_task(void)
+{
+    // Initialize scheduler
+    scheduler_init(100);
+
+    // Schedule one test task
+    schedule(TASK0, 5); // TASK0, every 5 SysTicks
+
+    // Execute task counter
+    for(unsigned char iTick = 0; iTick < 5; iTick++)
+        count_task(TASK0);
+    
+    // Check task flag
+    TEST_ASSERT_EQUAL_UINT8(1, run(TASK0));
+
+    // Check run flag again, it should be 0 now
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK0));
+
+    // Execute task counter longer than scheduled ticks
+    for(unsigned char iTick = 0; iTick < 7; iTick++)
+        count_task(TASK0);
+    
+    // Check task flag, it should stayed active
+    TEST_ASSERT_EQUAL_UINT8(1, run(TASK0));
+
+    // Test the overflow flag -> should be 0 since it is not used yet
+    TEST_ASSERT_EQUAL_UINT8(0, schedule_overflow());
+};
+
+/**
  * @brief Test the schedule using SysTicks as schedule.
  * @details unit test
  */
-void test_schedule_systick(void)
+void test_schedule_tick(void)
 {
     // Initialize scheduler
     scheduler_init(100);
 
     // Schedule two tasks
-    schedule(0, 2); // TASK 0, every 2 SysTicks
-    schedule(1, 4); // TASK 1, every 4 SysTicks
-    schedule(2, 3); // TASK 2, every 3 SysTicks -> not active!
-    set_task(2, INACTIVE);
+    schedule(TASK0, 2); // TASK 0, every 2 SysTicks
+    schedule(TASK1, 4); // TASK 1, every 4 SysTicks
+    schedule(TASK2, 3); // TASK 2, every 3 SysTicks -> not active!
+    set_task(TASK2, INACTIVE);
     
     // Test whether tasks are active and do not want to run
-    TEST_ASSERT_EQUAL_UINT8(ACTIVE, get_task(0));
-    TEST_ASSERT_EQUAL_UINT8(ACTIVE, get_task(1));
-    TEST_ASSERT_EQUAL_UINT8(INACTIVE, get_task(2));
-    TEST_ASSERT_EQUAL_UINT8(0, run(0));
-    TEST_ASSERT_EQUAL_UINT8(0, run(1));
-    TEST_ASSERT_EQUAL_UINT8(0, run(2));
+    TEST_ASSERT_EQUAL_UINT8(ACTIVE, get_task(TASK0));
+    TEST_ASSERT_EQUAL_UINT8(ACTIVE, get_task(TASK1));
+    TEST_ASSERT_EQUAL_UINT8(INACTIVE, get_task(TASK2));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK0));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK1));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK2));
 
     // Run scheduler and check task flags - Ticks == 0
     run_scheduler();
-    TEST_ASSERT_EQUAL_UINT8(0, run(0));
-    TEST_ASSERT_EQUAL_UINT8(0, run(1));
-    TEST_ASSERT_EQUAL_UINT8(0, run(2));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK0));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK1));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK2));
 
     // Run scheduler and check task flags - Ticks == 1
     run_scheduler();
-    TEST_ASSERT_EQUAL_UINT8(1, run(0));
-    TEST_ASSERT_EQUAL_UINT8(0, run(1));
-    TEST_ASSERT_EQUAL_UINT8(0, run(2));
+    TEST_ASSERT_EQUAL_UINT8(1, run(TASK0));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK1));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK2));
 
     // Run scheduler and check task flags - Ticks == 2
     run_scheduler();
-    TEST_ASSERT_EQUAL_UINT8(0, run(0));
-    TEST_ASSERT_EQUAL_UINT8(0, run(1));
-    TEST_ASSERT_EQUAL_UINT8(0, run(2));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK0));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK1));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK2));
 
     // Run scheduler and check task flags - Ticks == 3
     run_scheduler();
-    TEST_ASSERT_EQUAL_UINT8(1, run(0));
-    TEST_ASSERT_EQUAL_UINT8(1, run(1));
-    TEST_ASSERT_EQUAL_UINT8(0, run(2));
+    TEST_ASSERT_EQUAL_UINT8(1, run(TASK0));
+    TEST_ASSERT_EQUAL_UINT8(1, run(TASK1));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK2));
 
     // Run scheduler and check task flags - Ticks == 4
     run_scheduler();
-    TEST_ASSERT_EQUAL_UINT8(0, run(0));
-    TEST_ASSERT_EQUAL_UINT8(0, run(1));
-    TEST_ASSERT_EQUAL_UINT8(0, run(2));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK0));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK1));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK2));
 
     // Run scheduler and check task flags - Ticks == 5
     run_scheduler();
-    TEST_ASSERT_EQUAL_UINT8(1, run(0));
-    TEST_ASSERT_EQUAL_UINT8(0, run(1));
-    TEST_ASSERT_EQUAL_UINT8(0, run(2));
+    TEST_ASSERT_EQUAL_UINT8(1, run(TASK0));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK1));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK2));
+
+    // Test the overflow flag -> should be 0 since it is not used yet
+    TEST_ASSERT_EQUAL_UINT8(0, schedule_overflow());
 };
 
 /**
@@ -143,54 +179,119 @@ void test_schedule_us(void)
     scheduler_init(100);
 
     // Schedule two tasks
-    schedule_us(0, 200); // TASK 0, every 200 us
-    schedule_us(1, 400); // TASK 1, every 400 us
-    schedule_us(2, 300); // TASK 2, every 300 us -> not active!
-    set_task(2, INACTIVE);
+    schedule_us(TASK0, 200); // TASK 0, every 200 us
+    schedule_us(TASK1, 400); // TASK 1, every 400 us
+    schedule_us(TASK2, 300); // TASK 2, every 300 us -> not active!
+    set_task(TASK2, INACTIVE);
     
     // Test whether tasks are active and do not want to run
-    TEST_ASSERT_EQUAL_UINT8(ACTIVE, get_task(0));
-    TEST_ASSERT_EQUAL_UINT8(ACTIVE, get_task(1));
-    TEST_ASSERT_EQUAL_UINT8(INACTIVE, get_task(2));
-    TEST_ASSERT_EQUAL_UINT8(0, run(0));
-    TEST_ASSERT_EQUAL_UINT8(0, run(1));
-    TEST_ASSERT_EQUAL_UINT8(0, run(2));
+    TEST_ASSERT_EQUAL_UINT8(ACTIVE, get_task(TASK0));
+    TEST_ASSERT_EQUAL_UINT8(ACTIVE, get_task(TASK1));
+    TEST_ASSERT_EQUAL_UINT8(INACTIVE, get_task(TASK2));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK0));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK1));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK2));
 
     // Run scheduler and check task flags - Ticks == 000 us
     run_scheduler();
-    TEST_ASSERT_EQUAL_UINT8(0, run(0));
-    TEST_ASSERT_EQUAL_UINT8(0, run(1));
-    TEST_ASSERT_EQUAL_UINT8(0, run(2));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK0));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK1));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK2));
 
     // Run scheduler and check task flags - Ticks == 100 us
     run_scheduler();
-    TEST_ASSERT_EQUAL_UINT8(1, run(0));
-    TEST_ASSERT_EQUAL_UINT8(0, run(1));
-    TEST_ASSERT_EQUAL_UINT8(0, run(2));
+    TEST_ASSERT_EQUAL_UINT8(1, run(TASK0));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK1));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK2));
 
     // Run scheduler and check task flags - Ticks == 200 us
     run_scheduler();
-    TEST_ASSERT_EQUAL_UINT8(0, run(0));
-    TEST_ASSERT_EQUAL_UINT8(0, run(1));
-    TEST_ASSERT_EQUAL_UINT8(0, run(2));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK0));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK1));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK2));
 
     // Run scheduler and check task flags - Ticks == 300 us
     run_scheduler();
-    TEST_ASSERT_EQUAL_UINT8(1, run(0));
-    TEST_ASSERT_EQUAL_UINT8(1, run(1));
-    TEST_ASSERT_EQUAL_UINT8(0, run(2));
+    TEST_ASSERT_EQUAL_UINT8(1, run(TASK0));
+    TEST_ASSERT_EQUAL_UINT8(1, run(TASK1));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK2));
 
     // Run scheduler and check task flags - Ticks == 400 us
     run_scheduler();
-    TEST_ASSERT_EQUAL_UINT8(0, run(0));
-    TEST_ASSERT_EQUAL_UINT8(0, run(1));
-    TEST_ASSERT_EQUAL_UINT8(0, run(2));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK0));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK1));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK2));
 
     // Run scheduler and check task flags - Ticks == 500 us
     run_scheduler();
-    TEST_ASSERT_EQUAL_UINT8(1, run(0));
-    TEST_ASSERT_EQUAL_UINT8(0, run(1));
-    TEST_ASSERT_EQUAL_UINT8(0, run(2));
+    TEST_ASSERT_EQUAL_UINT8(1, run(TASK0));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK1));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK2));
+
+    // Test the overflow flag -> should be 0 since it is not used yet
+    TEST_ASSERT_EQUAL_UINT8(0, schedule_overflow());
+};
+
+/**
+ * @brief Test the schedule using ms as schedule.
+ * @details unit test
+ */
+void test_schedule_ms(void)
+{
+    // Initialize scheduler
+    scheduler_init(100);
+
+    // Schedule two tasks
+    schedule_ms(TASK0, 2); // TASK 0, every 2 ms
+    schedule_ms(TASK1, 4); // TASK 1, every 4 ms
+    schedule_ms(TASK2, 3); // TASK 2, every 3 ms -> not active!
+    set_task(TASK2, INACTIVE);
+    
+    // Test whether tasks are active and do not want to run
+    TEST_ASSERT_EQUAL_UINT8(ACTIVE, get_task(TASK0));
+    TEST_ASSERT_EQUAL_UINT8(ACTIVE, get_task(TASK1));
+    TEST_ASSERT_EQUAL_UINT8(INACTIVE, get_task(TASK2));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK0));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK1));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK2));
+
+    // Run scheduler and check task flags - Ticks == 1 ms
+    for(unsigned int iTick = 0; iTick < 10; iTick++)
+        run_scheduler();
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK0));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK1));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK2));
+
+    // Run scheduler and check task flags - Ticks == 2 ms
+    for(unsigned int iTick = 0; iTick < 10; iTick++)
+        run_scheduler();
+    TEST_ASSERT_EQUAL_UINT8(1, run(TASK0));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK1));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK2));
+
+    // Run scheduler and check task flags - Ticks == 3 ms
+    for(unsigned int iTick = 0; iTick < 10; iTick++)
+        run_scheduler();
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK0));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK1));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK2));
+
+    // Run scheduler and check task flags - Ticks == 4 ms
+    for(unsigned int iTick = 0; iTick < 10; iTick++)
+        run_scheduler();
+    TEST_ASSERT_EQUAL_UINT8(1, run(TASK0));
+    TEST_ASSERT_EQUAL_UINT8(1, run(TASK1));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK2));
+
+    // Run scheduler and check task flags - Ticks == 5 ms
+    for(unsigned int iTick = 0; iTick < 10; iTick++)
+        run_scheduler();
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK0));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK1));
+    TEST_ASSERT_EQUAL_UINT8(0, run(TASK2));
+
+    // Test the overflow flag -> should be 0 since it is not used yet
+    TEST_ASSERT_EQUAL_UINT8(0, schedule_overflow());
 };
 
 // ****** Main ******
@@ -199,7 +300,9 @@ int main(void)
     UNITY_BEGIN();
     RUN_TEST(test_init);
     RUN_TEST(test_set_task);
-    RUN_TEST(test_schedule_systick);
+    RUN_TEST(test_count_task);
+    RUN_TEST(test_schedule_tick);
     RUN_TEST(test_schedule_us);
+    RUN_TEST(test_schedule_ms);
     UNITY_END();
 };
